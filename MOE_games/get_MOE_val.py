abd32c5bdcee58eb3ac8e3a16340c7841f1a6215
Francis Moran <francis.moran@siriusxm.com>
2025-03-12 08:55:43 -0400

    Added plotting over bases to get_MOE_val

--- MOE_games/get_MOE_val.py
+++ MOE_games/get_MOE_val.py
@@ -1,4 +1,25 @@
 from MOE_n_bases import MOE_n_bases
 import numpy as np
+import matplotlib.pyplot as plt
 
-print(MOE_n_bases(6))
+x_vals = []
+unentangled_vals = []
+nonsignaling_vals = []
+quantum_vals = []
+
+for x in [6, 8]:
+    ue, ns, qu = MOE_n_bases(x)
+
+    x_vals.append(x)
+    unentangled_vals.append(ue)
+    nonsignaling_vals.append(ns)
+    quantum_vals.append(qu)
+
+# Plot results over x
+plt.plot(x_vals, unentangled_vals, marker='o', label="Unentangled Values")
+plt.plot(x_vals, nonsignaling_vals, marker='s', label="Non-Signaling Values")
+plt.plot(x_vals, quantum_vals, marker='d', label="Quantum Values")
+
+plt.xlabel("Num Bases")
+plt.legend()
+plt.show()
